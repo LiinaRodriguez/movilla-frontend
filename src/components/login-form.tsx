fb5b8b194c32827bb8b8980e7f5316d40c78314b
LinaRodriguez <lsrodriguez@unillanos.edu.co>
2025-05-13 23:25:32 -0500

    add login & signup form

---
+++ src/components/login-form.tsx
@@ -0,0 +1,68 @@
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle>Inicia Sesión en tu cuenta</CardTitle>
+          <CardDescription>
+           Ingresa tu correo para iniciar sesión
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </div>
+              <div className="grid gap-3">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Contraseña</Label>
+                  <a
+                    href="#"
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                  >
+                   ¿Olvidaste tu contraseña?
+                  </a>
+                </div>
+                <Input id="password" type="password" required />
+              </div>
+              <div className="flex flex-col gap-3">
+                <Button type="submit" className="w-full">
+                  Login
+                </Button>
+                
+              </div>
+            </div>
+            <div className="mt-4 text-center text-sm">
+              No tienes una cuenta?{" "}
+              <a href="/signup" className="underline underline-offset-4">
+                crea una aquí
+              </a>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
